37b2f546d77f7ee7de00489fb37fe853f8e0c4d3
kickscar <kickscar@gmail.com>
2016-11-03 15:19:45 +0900

    1st commit

---
+++ src/main/java/com/bit2016/guestbook/controller/GuestbookController.java
@@ -0,0 +1,47 @@
+package com.bit2016.guestbook.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bit2016.guestbook.dao.GuestbookDao;
+import com.bit2016.guestbook.vo.GuestbookVo;
+
+@Controller
+public class GuestbookController {
+	
+	@Autowired
+	private GuestbookDao guestbookDao;
+	
+	@RequestMapping( "" )
+	public String index( Model model ){
+		List<GuestbookVo> list = guestbookDao.getList();
+		model.addAttribute( "list", list );
+		return "/WEB-INF/views/index.jsp";
+	}
+	
+	@RequestMapping( "/deleteform/{no}" )
+	public String deleteform( @PathVariable( "no" ) Long no, Model model ){
+		model.addAttribute( "no", no );
+		return "/WEB-INF/views/deleteform.jsp";
+	}
+	
+	@RequestMapping( value="/delete", method=RequestMethod.POST )
+	public String delete( @ModelAttribute GuestbookVo vo ){
+		guestbookDao.delete( vo );
+		return "redirect:/";
+	}
+	
+	@RequestMapping( value="/add", method=RequestMethod.POST )
+	public String add( @ModelAttribute GuestbookVo vo ) {
+		guestbookDao.insert(vo);
+		return "redirect:/";
+	}
+}
